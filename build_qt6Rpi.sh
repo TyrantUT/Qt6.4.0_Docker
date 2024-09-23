a3168ed9eadc0318737f3d5fa0176a34fdb46d1d
TyrantUT <16949372+TyrantUT@users.noreply.github.com>
2024-09-23 12:58:32 -0700

    Update build_qt6Rpi.sh

--- build_qt6Rpi.sh
+++ build_qt6Rpi.sh
@@ -7,10 +7,12 @@ set -exuo pipefail
 
 BUILD_TARGET=/build/qt6_host
 SRC=/src
-QT_BRANCH="6.4.0"
+QT_BRANCH="6.7.2"
 DEBIAN_VERSION=$(lsb_release -cs)
-MAKE_CORES="$(expr $(nproc) + 2)"
+MAKE_CORES="$(expr $(nproc))"
 BUILD_TARGET_PI=/build/qt6_rpi
+INSTALL_PREFIX=/opt/qt6pi
+QT_HOST_PATH=/opt/qt6
 
 mkdir -p "$BUILD_TARGET_PI"
 
@@ -19,67 +21,83 @@ mkdir -p "$BUILD_TARGET_PI"
 function build_qtpi () {
 
     local SRC_DIR="/src/qt6pi"
-
     mkdir -p "$SRC_DIR"
-
     pushd "$BUILD_TARGET_PI"
 
-    "$SRC"/qt6/configure -qpa eglfs \
-            -confirm-license \
-            -release \
-            -qt-host-path /opt/qt6/6.4.0/gcc_64 \
-            -device-option CROSS_COMPILE=/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- \
-            -device linux-rasp-pi4-v3d-g++ \
-            -eglfs \
-            -extprefix "$SRC_DIR/qt6pi" \
-            -prefix /usr/local/qt5pi \
-            -pkg-config \
-            -qt-pcre \
-            -no-pch \
-            -evdev \
-            -system-freetype \
-            -fontconfig \
-            -glib \
-            -make libs \
-            -no-cups \
-            -no-gtk \
-            -no-use-gold-linker \
-            -nomake examples \
-            -nomake tests \
-            -opensource \
-            -skip qtwebengine \
-            -skip qtandroidextras \
-            -skip qtgamepad \
-            -skip qtlocation \
-            -skip qtlottie \
-            -skip qtmacextras \
-            -skip qtpurchasing \
-            -skip qtscxml \
-            -skip qtsensors \
-            -skip qtserialbus \
-            -skip qtserialport \
-            -skip qtspeech \
-            -skip qttools \
-            -skip qttranslations \
-            -skip qtvirtualkeyboard \
-            -skip qtwayland \
-            -skip qtwebview \
-            -skip qtwinextras \
-            -skip wayland \
-            -skip qtdoc \
-            -skip qtmultimedia \
-            -skip qtquick3d \
-            -skip qtquick3dphysics \
-            -sysroot /sysroot \
-            -- -DCMAKE_TOOLCHAIN_FILE=/usr/local/bin//toolchain.cmake \
-                -DQT_FEATURE_xcb=ON \
-                -DFEATURE_xcb_xlib=ON \
-                -DQT_FEATURE_xlib=ON \
-                -DQT_BUILD_EXAMPLES=FALSE \
-                -DQT_BUILD_TESTS=FALSE \
-                -DQT_DEBUG_FIND_PACKAGE=ON \
-                -DQt6_DIR=/opt/qt6/6.4.0/gcc_64/lib/cmake/Qt6 \
-                -DQT_ADDITIONAL_PACKAGES_PREFIX_PATH=/opt/qt6/6.4.0/gcc_64
+    "$SRC"/qt6/configure \
+        -confirm-license \
+        -release \
+        -qpa eglfs \
+        -opengl es2 \
+        -eglfs \
+        -no-cups \
+        -nomake tests \
+        -qt-host-path $QT_HOST_PATH \
+        -extprefix $SRC_DIR \
+        -prefix $INSTALL_PREFIX \
+        -nomake examples \
+        -pkg-config \
+        -qt-pcre \
+        -no-pch \
+        -evdev \
+        -fontconfig \
+        -glib \
+        -shared \
+        -no-dbus \
+        -no-cups \
+        -no-gtk \
+        -no-use-gold-linker \
+        -opensource \
+        -skip qttranslations \
+        -skip qtdoc \
+        -skip qttools \
+        -skip qtwebengine \
+        -skip qtwebview \
+        -skip qtsensors \
+        -skip qtvirtualkeyboard \
+        -skip qtwebchannel \
+        -skip qtspeech \
+        -skip qtsql \
+        -skip qtdbus \
+        -skip qtxml \
+        -skip qtjpeg \
+        -skip qtlanguageserver \
+        -skip qtwebsockets \
+        -skip qthttpserver \
+        -skip qtserialport \
+        -skip qtpositioning \
+        -skip qtlocation \
+        -skip qtlottie \
+        -skip qtmqtt \
+        -skip qtremoteobjects \
+        -skip qtserialbus \
+        -skip qtsvg \
+        -skip qtwayland \
+        -skip qtcoap \
+        -skip qt5compat \
+        -skip qtconnectivity \
+        -skip qtrpc \
+        -skip qtimageformats \
+        -skip qtopcua \
+        -skip qtnetworkauth \
+        -skip qtactiveqt \
+        -skip qtgrpc \
+        -skip qtscxml \
+        -device linux-rasp-pi4-v3d-g++ \
+        -device-option CROSS_COMPILE=aarch64-linux-gnu- \
+        -- -DCMAKE_TOOLCHAIN_FILE=/build/toolchain.cmake \
+        -DQT_FEATURE_xcb=ON \
+        -DFEATURE_xcb_xlib=ON \
+        -DQT_FEATURE_xlib=ON \
+        -DQT_BUILD_EXAMPLES=FALSE \
+        -DQT_BUILD_TESTS=FALSE \
+        -DQT_DEBUG_FIND_PACKAGE=ON \
+        -DQT_FEATURE_egl=ON \
+        -DQT_FEATURE_opengl=ON \
+        -DQT_FEATURE_opengles2=ON \
+        -DQT_FEATURE_opengl_desktop=OFF \
+        -DQT_FEATURE_opengl_dynamic=OFF \
+        -DQT_HOST_PATH=$QT_HOST_PATH
 
 
     /usr/games/cowsay -f tux "Making QT Pi version $QT_BRANCH."
